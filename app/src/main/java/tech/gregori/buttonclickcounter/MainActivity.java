031fc78f7c895311b56f4ffce81338aa9342de60
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2020-10-19 21:54:16 -0300

    Armazenando o estado do programa

--- app/src/main/java/tech/gregori/buttonclickcounter/MainActivity.java
+++ app/src/main/java/tech/gregori/buttonclickcounter/MainActivity.java
@@ -1,9 +1,11 @@
 package tech.gregori.buttonclickcounter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button button;
     private TextView textView;
-    private int numTimesClicked = 0; // quantas vezes o botão foi clicado?
+    private static final String TEXT_CONTENTS = "TextContents"; // constante "chave"
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -47,5 +51,67 @@ public class MainActivity extends AppCompatActivity {
 
         // atribui o "listener" criado ao botão
         button.setOnClickListener(onClickListener);
+        Log.d(TAG, "onCreate: out");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        // buscar valor contido na chave TEXT_CONTENTS do armazenamento local do celular
+        String savedString = savedInstanceState.getString(TEXT_CONTENTS);
+        textView.setText(savedString); // insere a string no textview
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        // armazena o conteúdo da textview no armazenamento local do celular, na chave TEXT_CONTENTS
+        outState.putString(TEXT_CONTENTS, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
     }
 }
